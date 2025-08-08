2f9724c0dd836f617a8ae3c20b9e15a3c2a8c454
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-08-08 21:49:32 +0200

    Update

--- Plugin.cs
+++ Plugin.cs
@@ -10,7 +10,7 @@ using PlayerEncumbranceBar.Patches;
 namespace PlayerEncumbranceBar
 {
     // the version number here is generated on build and may have a warning if not yet built
-    [BepInPlugin("com.mpstark.PlayerEncumbranceBar", "PlayerEncumbranceBar", "1.1.2")]
+    [BepInPlugin("com.mpstark.PlayerEncumbranceBar", "PlayerEncumbranceBar", "1.1.3")]
     public class Plugin : BaseUnityPlugin
     {
         public static Plugin Instance;
